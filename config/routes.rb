11c6c52d8fe1b5ef581f6c334586bf15dc23fc4e
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-10-02 07:05:54 +0000

    [new]imitate withdrawal

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'homes/top'
+  root 'homes#top'
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  # 退会確認画面
+  get '/users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+  # 論理削除用のルーティング
+  patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
